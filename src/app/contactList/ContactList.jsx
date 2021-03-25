7dc6cbb409a45d2014901b50c79e25fe10dcc8e3
Ebenezer Don <ebenezerdonu@gmail.com>
2021-03-25 10:11:50 +0100

    use fetched data

--- src/app/contactList/ContactList.jsx
+++ src/app/contactList/ContactList.jsx
@@ -1,15 +1,48 @@
+import { useState, useMemo } from 'react'
 import useFetch from 'react-fetch-hook'
+import config from '../../configJson'
+import './contactList.scss'
 
 const ContactList = () => {
-  const { isLoading, data, error } = useFetch('https://api.randomuser.me/?results=120')
+  const url = `${config.userUrl}/?results=${config.numberCards}`
+  const { isLoading, data, error } = useFetch(url)
+  const [contactData, setContactData] = useState(null)
 
-  console.log(data?.results)
+  const filterByName = (data, value) => {
+    return data?.results?.filter(person => {
+      const firstLetter = person?.name?.last?.charAt(0)
+      return value.toLowerCase() === firstLetter.toLowerCase()
+    })
+  }
+
+  const tabs = (
+    config.tabs.map(value => {
+      const filteredData = filterByName(data, value)
+      const doStuff = () => {
+        setContactData(filteredData)
+        console.log(contactData)
+      }
+
+      return (
+        <div className="tab">
+          <button onClick={doStuff}>{value}</button>
+          <p>{filteredData?.length}</p>
+        </div>
+      )
+    })
+  )
+
+  // console.log(data?.results)
 
   return (
     <div className="contactList">
+      {tabs}
       {isLoading && <div>Loading...</div>}
       {data && <div>Hey</div>}
       {error && <div>Error fetching data...</div>}
+      {contactData?.map(contact => (
+        <div>{contact.name.last}</div>
+      ))}
     </div>
   )
 }
---
+++ src/configJson.js
@@ -0,0 +1,8 @@
+const configJson = {
+  "title": "Contact List",
+  "userUrl": "https://api.randomuser.me",
+  "numberCards": 120,
+  "tabs": ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+}
+
+export default configJson
