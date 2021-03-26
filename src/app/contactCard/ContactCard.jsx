379745bec6821f52ff5353403d885fef0abed00c
Ebenezer Don <ebenezerdonu@gmail.com>
2021-03-26 17:56:40 +0100

    add contact card functionality

---
+++ src/app/contactCard/ContactCard.jsx
@@ -0,0 +1,31 @@
+import { useEffect } from 'react'
+import './contactCard.scss'
+
+const ContactCard = ({ mousePosition, contact }) => {
+  useEffect(() => {
+    const contactCard = document.querySelector('.contactCard')
+    contactCard.style.left = (mousePosition[0] / 2) + 'px'
+    contactCard.style.top = (mousePosition[1] / 2) + 'px'
+  }, [mousePosition])
+
+  return (
+    <div className="contactCard">
+      <div className="contactCard__photo">
+        <img src="https://randomuser.me/api/portraits/women/55.jpg" alt="contact" />
+      </div>
+      <div>
+        <p className="contactCard__name">{contact?.name?.first.toUpperCase()}, {contact?.name?.last}</p>
+        <div className="contactCard__details">
+          <p><strong>e-mail</strong> {contact?.email}</p>
+          <p><strong>phone</strong> {contact?.cell}</p>
+          <p><strong>street</strong> {contact?.location?.street?.name}</p>
+          <p><strong>city</strong> {contact?.location?.city}</p>
+          <p><strong>state</strong> {contact?.location?.state}</p>
+          <p><strong>postcode</strong> {contact?.location?.postcode}</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ContactCard
--- src/app/contactList/ContactList.jsx
+++ src/app/contactList/ContactList.jsx
@@ -2,22 +2,45 @@ import { useState } from 'react'
 import useFetch from 'react-fetch-hook'
 import config from '../../configJson'
 import Tabs from './Tabs'
+import { ContactCard } from '../contactCard'
 import './contactList.scss'
 
 const ContactList = () => {
   const url = `${config.userUrl}/?results=${config.numberCards}`
   const { isLoading, data, error } = useFetch(url)
   const [contactData, setContactData] = useState(null)
+  const [cardIsOpen, setCardIsOpen] = useState(false)
+  const [mousePosition, setMousePosition] = useState([])
+  const [currentContactData, setCurrentContactData] = useState({})
   data && console.log(data.results)
 
+  const handleListButtonClick = (event, contact) => {
+    setMousePosition([event.clientX, event.clientY])
+    setCurrentContactData(contact)
+    setCardIsOpen(true)
+  }
+
+  const closeContactCard = ({target, currentTarget}) => {
+    if (target === currentTarget) setCardIsOpen(false)
+  }
+
   return (
-    <div className="contactList">
+    <div className="contactList" onClick={event => closeContactCard(event)}>
       <Tabs data={data} setContactData={setContactData}/>
       {isLoading && <div>Loading...</div>}
       {error && <div>Error fetching data...</div>}
+      {cardIsOpen && (
+        <ContactCard
+          mousePosition={mousePosition}
+          contact={currentContactData}
+        />
+      )}
       <div className="contactList__content">
         {contactData?.map((contact, index) => (
-          <button key={index} className="contactList__content__name">
+          <button
+            onClick={(event) => handleListButtonClick(event, contact)}
+            key={index} className="contactList__content__name"
+          >
             {contact.name.last}, {contact.name.first.toUpperCase()}
           </button>
         ))}
